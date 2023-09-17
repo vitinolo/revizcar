241973814b5721afd867ea9b6497981db28fcec7
thibaut lenoir <lenoir677@gmail.com>
2023-09-17 08:43:24 +0200

    affichage des cars

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -1,18 +1,19 @@
 <?php
 
 namespace App\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CarRepository; //appel repository
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CarController extends AbstractController
 {
     #[Route('/', name: 'app_car')]
-    public function index(): Response
+    public function index(CarRepository $repo): Response 
     {
+        $car = $repo->findBy([], ['createdAt' => 'DESC'], 10);
         return $this->render('car/car.html.twig', [
-            'controller_name' => 'CarController',
+            'car' => $car
         ]);
     }
 }
